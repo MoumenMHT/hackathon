2f0067ba870835a5f59d640ba6574b08caf648e2
moumen <moumenabdou482@gmail.com>
2025-11-08 13:52:00 +0100

    navBar and main content

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-full min-h-screen bg-gray-50">
+    <!-- Navigation Bar -->
+    <Navbar />
+    
+    <!-- Spacer for fixed navbar -->
+    
+    <!-- Main Content -->
+    <MainContent />
+  </div>
+</template>
+
+<script>
+import Navbar from './layout/home/Navbar.vue'
+import MainContent from './layout/home/MainContent.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+    MainContent
+  }
+}
+</script>
+                
